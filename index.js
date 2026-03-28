d976ba2b090ab0fb4c80a86d8cfbb52e53bbe039
Jagadeeshan <jagadeeshandharuman@fabc.global>
2026-03-28 09:05:40 +0530

    feat: add CI pipeline

---
+++ index.js
@@ -0,0 +1,3 @@
+console.log("Hello, DevOps Git Actions!");
+console.log("This is a sample code snippet for Kubernetes operations.");
+
---
+++ test.js
@@ -0,0 +1,6 @@
+console.log("Hello, starting tests!");
+setTimeout(() => {
+    console.log("waiting 2 sec!");
+}, 2000);
+
+console.log("Tests completed!");
